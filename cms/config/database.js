28640a1c6492f630d4a10e7cf9d6540234edaea5
Ketan Chavan <ketan2.chavan@ril.com>
2026-03-11 16:40:03 +0530

    feat: Migrate CMS database to PostgreSQL and configure Netlify deployment for the frontend.

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,12 +1,17 @@
 const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "sqlite",
+module.exports = ({ env }) => {
+  return {
     connection: {
-      filename: path.join(__dirname, "..", env("DATABASE_FILENAME", ".tmp/data.db")),
-    },
-    useNullAsDefault: true,
-  },
-});
+      client: "postgres",
+      connection: {
+        connectionString: env("DATABASE_URL", ""),
+        // ssl: env.bool("DATABASE_SSL", false) ? {
+        //   rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", false)
+        // } : false,
+      },
 
+      // acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    },
+  };
+};
